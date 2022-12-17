ff28efdf59c3d3087373a189a236744b358a5b82
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-12-17 11:32:36 -0500

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  // params: { countryIds: "US" },
+  headers: {
+    "X-RapidAPI-Key": "d9bfc8caefmsh71d420728fa230ap187fa6jsnf826d7f15999",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3f4c7d14daab872155f896009f745a0a";
